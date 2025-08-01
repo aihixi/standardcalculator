a9d86d5109b0adbe409154b23da735554872d99c
aihixi <162949415+aihixi@users.noreply.github.com>
2025-08-02 02:39:14 +0800

    减少组件

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,14 +1,15 @@
 import { createApp } from 'vue';
-import TDesign from 'tdesign-vue-next';
 import App from './App.vue';
 
 // 引入组件库的少量全局样式变量
 import './assets/main.css';
 import 'tdesign-vue-next/es/style/index.css';
-
+import ButtonPlugin from 'tdesign-vue-next';
+import CardPlugin from 'tdesign-vue-next/es/card';
 import router from './router'
 
 const app = createApp(App);
-app.use(TDesign);
+app.use(ButtonPlugin);
+app.use(CardPlugin);
 app.use(router);
 app.mount('#app')
